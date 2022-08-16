96908baf5afca827e4af83580efb6c5d7f094d72
hyeyoon0206 <hyeyoon0206@naver.com>
2022-08-16 22:53:06 +0900

    fix: 튜토리얼 멘트 수정

--- Colorful_Adventure/Assets/Scripts/레벨선택창 1,2씬 다른 적용/LevelManager.cs
+++ Colorful_Adventure/Assets/Scripts/레벨선택창 1,2씬 다른 적용/LevelManager.cs
@@ -10,7 +10,7 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.DeleteAll(); //씬 잘 돌아가나 한번씩 확인하려면 잠금 해제 레벨 초기화 필요하므로 그때마다 이거 살려서 프로그램 돌리면 됨
+        //PlayerPrefs.DeleteAll(); //씬 잘 돌아가나 한번씩 확인하려면 잠금 해제 레벨 초기화 필요하므로 그때마다 이거 살려서 프로그램 돌리면 됨
         levelsUnlocked = PlayerPrefs.GetInt("levelsUnlocked", 12);
         for(int i = 0; i < buttons.Length; i++) 
         {
